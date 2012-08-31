1afcfaf0eeaf18a598d3b0cc8b39e0b3ad0769cb
dominic123321 <irgendwas@uibk.ac.at>
2012-08-31 14:49:48 +0000

    git-svn-id: file:///home/seb/testrepo/trunk@111 e7be941a-b4c0-0410-90d9-ef316cc3cf24

--- src/qualityAssurance/rules/CheckForSampleRSRSAligned.java
+++ src/qualityAssurance/rules/CheckForSampleRSRSAligned.java
@@ -1,6 +1,17 @@
 package qualityAssurance.rules;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 import qualityAssurance.QualityAssistent;
 import qualityAssurance.QualityError;
 import core.Polymorphism;
@@ -8,26 +19,63 @@ import core.TestSample;
 import exceptions.parse.sample.InvalidPolymorphismException;
 
 public class CheckForSampleRSRSAligned implements HaplogrepRule {
-
-	@Override
-	public void evaluate(QualityAssistent qualityAssistent, TestSample currentSample) {
-		
+	static ArrayList<Polymorphism> uniqueRSRSPolys = null;
+	
+	public CheckForSampleRSRSAligned(){
+		if(uniqueRSRSPolys == null){
+			uniqueRSRSPolys = new ArrayList<Polymorphism>();
+			
+			loadUniqueRSRSPositions();
+		}
+	}
+	
+	private void loadUniqueRSRSPositions() {
 		try {
-			if(!currentSample.getSample().contains(new Polymorphism("263A")) && 
-					!currentSample.getSample().contains(new Polymorphism("8860A")) &&
-					!currentSample.getSample().contains(new Polymorphism("15326A"))){
-				qualityAssistent.addNewIssue(new QualityError(qualityAssistent, currentSample, "Common RSRS polymorphims (263A, 8860A or 15326A) found! " +
-						"The sample seems to be aligned to RSRS. Haplogrep only supports rCRS aligned samples."));
+			InputStream phyloFile = this.getClass().getClassLoader().getResourceAsStream("RSRSPolymorphisms");
+			
+			if(phyloFile == null)
+				phyloFile = new  FileInputStream(new File("testDataFiles/RSRSPolymorphisms"));
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(phyloFile));
+
+			String currentLine = reader.readLine();
+
+			while(currentLine != null){
+			Polymorphism newUniquePoly = new Polymorphism(currentLine.trim());
+			uniqueRSRSPolys.add(newUniquePoly);
+			currentLine = reader.readLine();
 			}
 			
-					
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (InvalidPolymorphismException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			
-		
 
 	}
+	@Override
+	public void evaluate(QualityAssistent qualityAssistent, TestSample currentSample) {
+
+		int numRSRSPolysFound = 0;
+	
+			
+		for(Polymorphism currentUniqueRSRSPoly : uniqueRSRSPolys)	
+			if(currentSample.getSample().contains(currentUniqueRSRSPoly)) 
+				numRSRSPolysFound++;
+		
+		
+			if(numRSRSPolysFound > 1){
+				qualityAssistent.addNewIssue(new QualityError(qualityAssistent, currentSample, numRSRSPolysFound + " common RSRS polymorphims found! " +
+						"The sample seems to be aligned to RSRS. Haplogrep only supports rCRS aligned samples."));
+			}
+			
+					
+		} 
 
 }
