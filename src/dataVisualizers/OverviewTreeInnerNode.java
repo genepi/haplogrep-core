0d5ff69866b0bd1bd8b7efc3cb84fd0c853939a4
dominic123321 <irgendwas@uibk.ac.at>
2013-11-21 18:27:38 +0000

    Phylogenizer now shows expected and not onyl found polys
    git-svn-id: file:///home/seb/testrepo/trunk@170 e7be941a-b4c0-0410-90d9-ef316cc3cf24

--- src/dataVisualizers/OverviewTreeInnerNode.java
+++ src/dataVisualizers/OverviewTreeInnerNode.java
@@ -6,7 +6,7 @@ import search.SearchResultTreeNode;
 import core.Polymorphism;
 
 public class OverviewTreeInnerNode extends TreeNode {
-	private ArrayList<Polymorphism> foundPolys = new ArrayList<Polymorphism>();
+	private ArrayList<Polymorphism> expectedPolys = new ArrayList<Polymorphism>();
 
 	public OverviewTreeInnerNode(TreeNode parent,SearchResultTreeNode attachedPhylotreeNode){
 		if(parent != null)
@@ -14,19 +14,19 @@ public class OverviewTreeInnerNode extends TreeNode {
 		
 		this.parent = parent;
 		phylotreeNode = attachedPhylotreeNode.getPhyloTreeNode();
-		foundPolys.addAll(attachedPhylotreeNode.getFoundPolys());
+		expectedPolys.addAll(attachedPhylotreeNode.getExpectedPolys());
 	}
 	
 	public void addDistinctFoundPolys(ArrayList<Polymorphism> foundPolysToAddDistinct) {
 		for(Polymorphism currentPolyToAdd : foundPolysToAddDistinct){
-			if(!foundPolys.contains(currentPolyToAdd))
-				foundPolys.add(currentPolyToAdd);
+			if(!expectedPolys.contains(currentPolyToAdd))
+				expectedPolys.add(currentPolyToAdd);
 		}
 		
 	}
 	
 	public ArrayList<Polymorphism> getExpectedPoly() {
-		return foundPolys;
+		return expectedPolys;
 	}
 
 }
