ee31cc65a23f08649170d569405933a66badd0d4
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2018-10-22 14:31:15 +0200

    Calculate stats from verifybamID

--- tests/contamination/ContaminationTests.java
+++ tests/contamination/ContaminationTests.java
@@ -2,29 +2,17 @@ package contamination;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-
 import org.junit.Test;
-
 import contamination.Contamination.Status;
 import contamination.objects.Sample;
-import core.Polymorphism;
 import core.SampleFile;
-import core.SampleRanges;
-import core.TestSample;
 import genepi.io.FileUtil;
 import genepi.io.table.reader.CsvTableReader;
 import phylotree.Phylotree;
 import phylotree.PhylotreeManager;
-import search.ranking.results.RankedResult;
 
 public class ContaminationTests {
 
@@ -71,7 +59,7 @@ public class ContaminationTests {
 
 		assertEquals(26, count);
 
-		//FileUtil.deleteFile(output);
+		FileUtil.deleteFile(output);
 
 	}
 
@@ -80,7 +68,7 @@ public class ContaminationTests {
 
 		Phylotree phylotree = PhylotreeManager.getInstance().getPhylotree("phylotree17.xml", "weights17.txt");
 		String folder = "test-data/contamination/1000G/high-free-mix/";
-		String variantFile = folder +"high-free-mix-1000G.txt";
+		String variantFile = folder + "high-free-mix-1000G.txt";
 		String output = folder + "chip-mix-report.txt";
 		MutationServerReader reader = new MutationServerReader(variantFile);
 		HashMap<String, Sample> mutationServerSamples = reader.parse();
@@ -113,7 +101,7 @@ public class ContaminationTests {
 
 		assertEquals(7, count);
 
-		//FileUtil.deleteFile(output);
+		FileUtil.deleteFile(output);
 
 	}
 
@@ -123,7 +111,7 @@ public class ContaminationTests {
 		Phylotree phylotree = PhylotreeManager.getInstance().getPhylotree("phylotree17.xml", "weights17.txt");
 		String folder = "test-data/contamination/1000G/no-contamination/";
 		String variantFile = folder + "no-contamination-1000G.txt";
-		String output = folder +"no-contamination-report.txt";
+		String output = folder + "no-contamination-report.txt";
 
 		Contamination contamination = new Contamination();
 
@@ -201,10 +189,10 @@ public class ContaminationTests {
 
 		assertEquals(0, count);
 
-		//FileUtil.deleteFile(output);
+		FileUtil.deleteFile(output);
 
 	}
-	
+
 	@Test
 	public void testBaq1000G() throws Exception {
 
@@ -212,8 +200,6 @@ public class ContaminationTests {
 		String folder = "test-data/contamination/1000G/final-samples/";
 		String variantFile = folder + "1000G_BAQ.txt";
 		String out = folder + "1000g-report.txt";
-		String verifyBam = "test-data/contamination/1000G/verifybam/verifybam-1000G.txt";
-		String verifyOut = folder + "1000g-report-verifybam.txt";
 
 		MutationServerReader reader = new MutationServerReader(variantFile);
 		HashMap<String, Sample> mutationServerSamples = reader.parse();
@@ -252,41 +238,11 @@ public class ContaminationTests {
 
 		// FileUtil.deleteFile(out);
 
-		CsvTableReader readerVerifyBam = new CsvTableReader(verifyBam, '\t');
-		CsvTableReader readerContamination = new CsvTableReader(out, '\t');
-		FileWriter writer = new FileWriter(verifyOut);
-		writer.write("SAMPLE" +"\t"+ "CONT_FREE" + "\t" + "CONT_MIX"+"\t" + "MINOR_LEVEL" + "\t" + "STATUS" +"\n");
-		HashMap<String, String> samples = new HashMap<String, String>();
-		
-		
-		while (readerContamination.next()) {
-			String id = readerContamination.getString("SampleID");
-			id = id.split("\\.",2)[0];
-			String level = readerContamination.getString("MinorMeanHetLevel");
-			String status = readerContamination.getString("Contamination");
-			
-			if(status.contains("HIGH")) {
-			samples.put(id, level + "\t" + status);
-			}
-		}
-
-		while (readerVerifyBam.next()) {
-			String id = readerVerifyBam.getString("ID");
-			String free = readerVerifyBam.getString("free_contam");
-			String chip = readerVerifyBam.getString("chip_contam");
-			String add = samples.get(id);
-			
-			if(add!=null){
-			writer.write(id + "\t" + free + "\t" + chip + "\t" + add+"\n");
-			}
-		}
-		writer.close();
-		
 		assertEquals(121, countHigh);
 		assertEquals(29, countLow);
 
 	}
-	
+
 	@Test
 	public void testNoBaq1000G() throws Exception {
 
@@ -294,9 +250,6 @@ public class ContaminationTests {
 		String folder = "test-data/contamination/1000G/final-samples/";
 		String variantFile = folder + "1000G_NOBAQ.txt";
 		String out = folder + "1000G_NOBAQ_report.txt";
-		String verifyBam = "test-data/contamination/1000G/verifybam/verifybam-1000G.txt";
-		String verifyOut = folder + "1000g-report-verifybam.txt";
-		FileWriter writer = new FileWriter(verifyOut);
 
 		MutationServerReader reader = new MutationServerReader(variantFile);
 		HashMap<String, Sample> mutationServerSamples = reader.parse(0.00);
@@ -333,29 +286,8 @@ public class ContaminationTests {
 			}
 		}
 
-		// FileUtil.deleteFile(out);
+		FileUtil.deleteFile(out);
 
-		CsvTableReader reader1000G = new CsvTableReader(verifyBam, '\t');
-		CsvTableReader readerContamination = new CsvTableReader(out, '\t');
-		writer.write("SAMPLE" +"\t"+ "CONT_FREE" + "\t" + "CONT_MIX"+"\t" + "MINOR_LEVEL" + "\t" + "STATUS" +"\n");
-		HashMap<String, String> samples = new HashMap<String, String>();
-		while (readerContamination.next()) {
-			String id = readerContamination.getString("SampleID");
-			id = id.split("\\.",2)[0];
-			String level = readerContamination.getString("MinorMeanHetLevel");
-			String status = readerContamination.getString("Contamination");
-			samples.put(id, level + "\t" + status);
-		}
-
-		while (reader1000G.next()) {
-			String id = reader1000G.getString("ID");
-			String free = reader1000G.getString("free_contam");
-			String chip = reader1000G.getString("chip_contam");
-			String add = samples.get(id);
-			writer.write(id + "\t" + free + "\t" + chip + "\t" + add+"\n");
-		}
-		writer.close();
-		
 		assertEquals(117, countHigh);
 		assertEquals(28, countLow);
 
