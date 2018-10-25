5d9834ded1e0a03d355c8c6f642c1c1f79b5fcfb
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2018-10-25 16:12:45 +0200

    Remove unused import

--- tests/contamination/MutationServerReaderTests.java
+++ tests/contamination/MutationServerReaderTests.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 
 import org.junit.Test;
 
