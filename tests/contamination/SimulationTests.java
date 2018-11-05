1e4668a355e6116730e7360eddf50a9b7b25ef98
seppinho <sebastian.schoenherr@i-md.ac.at>
2018-11-05 13:21:44 +0100

    add simulation tests

---
+++ tests/contamination/SimulationTests.java
@@ -0,0 +1,67 @@
+package contamination;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import org.junit.Test;
+import contamination.Contamination.Status;
+import contamination.objects.Sample;
+import core.SampleFile;
+import genepi.io.FileUtil;
+import genepi.io.table.reader.CsvTableReader;
+import importer.VcfImporter;
+import phylotree.Phylotree;
+import phylotree.PhylotreeManager;
+
+public class SimulationTests {
+
+	@Test
+	public void testSimulation() throws Exception {
+
+		Phylotree phylotree = PhylotreeManager.getInstance().getPhylotree("phylotree17.xml", "weights17.txt");
+		String folder = "test-data/contamination/simulation/";
+		String variantFile = folder + "vcf.vcf";
+		String output = folder + "chip-mix-report.txt";
+
+		VariantSplitter splitter = new VariantSplitter();
+
+		VcfImporter reader = new VcfImporter();
+
+		HashMap<String, Sample> mutationServerSamples = reader.load(new File(variantFile), false);
+
+		ArrayList<String> profiles = splitter.split(mutationServerSamples);
+
+		HashSet<String> set = new HashSet<String>();
+
+		String[] splits = profiles.get(0).split("\t");
+
+		for (int i = 3; i < splits.length; i++) {
+			set.add(splits[i]);
+		}
+
+		Contamination contamination = new Contamination();
+
+		HaplogroupClassifier classifier = new HaplogroupClassifier();
+		SampleFile haplogrepSamples = classifier.calculateHaplogrops(phylotree, profiles);
+
+		contamination.calcContamination(mutationServerSamples, haplogrepSamples.getTestSamples(), output);
+
+		CsvTableReader readerOut = new CsvTableReader(output, '\t');
+
+		int count = 0;
+		while (readerOut.next()) {
+			if (readerOut.getString("Contamination").equals(Status.HIGH.name())) {
+				count++;
+
+			}
+		}
+
+		assertEquals(26, count);
+
+		FileUtil.deleteFile(output);
+
+	}
+}
