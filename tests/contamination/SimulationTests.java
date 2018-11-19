870ef08077faff84a4944dbc6d17c09763ef3248
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2018-11-19 16:15:29 +0100

    remove unused files

--- tests/contamination/SimulationTests.java
+++ tests/contamination/SimulationTests.java
@@ -27,7 +27,7 @@ public class SimulationTests {
 
 		File[] files = new File(folder).listFiles(new FilenameFilter() {
 			public boolean accept(File dir, String name) {
-				return name.toLowerCase().equals("5.vcf.gz");
+				return name.toLowerCase().endsWith(".vcf.gz");
 			}
 		});
 
@@ -54,74 +54,89 @@ public class SimulationTests {
 				HaplogroupClassifier classifier = new HaplogroupClassifier();
 				SampleFile haplogrepSamples = classifier.calculateHaplogrops(phylotree, profiles);
 				int setup = 0;
-				for (double qual = 0.5; qual <= 0.5; qual += 0.05) {
-					for (int h = 10; h < 40; h++) {
-						for (int w = 0; w <= 5; w++) {
-							setup++;
-							int noise = Integer.valueOf(file.getName().substring(0, file.getName().length() - 7));
-							String output = folder + noise + "_" + qual + "_" + h + "_" + w + ".txt";
-							Contamination contamination = new Contamination();
-							contamination.setSettingHgQuality(qual);
-							contamination.setSettingAmountHigh(h);
-							contamination.setSettingAmountLow((h - w) < 0 ? 0 : (h - w));
-							contamination.detect(mutationServerSamples, haplogrepSamples.getTestSamples(), output);
+				// for (double qual = 0.5; qual <= 0.5; qual += 0.05) {
+				for (int h = 10; h < 15; h++) {
+					for (int w = 0; w <= 5; w++) {
+						setup++;
+						int noise = Integer.valueOf(file.getName().substring(0, file.getName().length() - 7));
+						String output = folder + noise + "_0.5" + "_" + h + "_" + w + ".txt";
+						Contamination contamination = new Contamination();
+						contamination.setSettingHgQuality(0.5);
+						contamination.setSettingAmountHigh(h);
+						contamination.setSettingAmountLow((h - w) < 0 ? 0 : (h - w));
+						contamination.detect(mutationServerSamples, haplogrepSamples.getTestSamples(), output);
 
-							CsvTableReader readerOut = new CsvTableReader(output, '\t');
+						CsvTableReader readerOut = new CsvTableReader(output, '\t');
 
-							int truePositives = 0;
-							int falsePositives = 0;
-							int trueNegatives = 0;
-							int falseNegative = 0;
-							int count = 0;
-							int distance = 0;
-							int countCont = 0;
-							while (readerOut.next()) {
-								count++;
-								String id = readerOut.getString("SampleID");
-								String status = readerOut.getString("Contamination");
-								distance += readerOut.getInteger("HG_Distance");
+						int truePositives = 0;
+						int falsePositives = 0;
+						int trueNegatives = 0;
+						int falseNegative = 0;
+						int count = 0;
+						int distance = 0;
+						int countCont = 0;
+						while (readerOut.next()) {
+							count++;
+							String id = readerOut.getString("SampleID");
+							String status = readerOut.getString("Contamination");
+							distance += readerOut.getInteger("HG_Distance");
 
-								String[] idSplits = id.split("_");
-								boolean cont;
-								if (idSplits[0].equals(idSplits[1])) {
-									cont = false;
-								} else {
-									countCont++;
-									cont = true;
-								}
-
-								if ((status.equals("HIGH") || status.equals("LOW")) && cont) {
-									truePositives++;
-								} else if (status.equals("NONE") && !cont) {
-									trueNegatives++;
-								} else if ((status.equals("HIGH") || status.equals("LOW")) && !cont) {
-									falsePositives++;
-								} else if (status.equals("NONE") && cont) {
-									falseNegative++;
-								}
-
+							String[] idSplits = id.split("_");
+							boolean cont;
+							if (idSplits[0].equals(idSplits[1])) {
+								cont = false;
+							} else {
+								countCont++;
+								cont = true;
 							}
-							System.out.println("cont " + countCont);
-							//new File(output).delete();
 
-							double sens = (double) truePositives / (truePositives + falseNegative);
-							double spec = (double) trueNegatives / (trueNegatives + falsePositives);
-							double prec = (double) truePositives / (truePositives + falsePositives);
-							double averageDistance = (double) distance / count;
-							writer.setDouble(0, setup);
-							writer.setDouble(1, count);
-							writer.setInteger(2, noise);
-							writer.setDouble(3, averageDistance);
-							writer.setDouble(4, sens);
-							writer.setDouble(5, spec);
-							writer.setDouble(6, prec);
-							writer.setDouble(7, qual);
-							writer.setInteger(8, h);
-							writer.setInteger(9, w);
-							writer.next();
+							if ((status.equals("HIGH") || status.equals("LOW")) && cont) {
+								truePositives++;
+							} else if (status.equals("NONE") && !cont) {
+								trueNegatives++;
+							} else if ((status.equals("HIGH") || status.equals("LOW")) && !cont) {
+								falsePositives++;
+							} else if (status.equals("NONE") && cont) {
+								falseNegative++;
+							}
+
 						}
+						// new File(output).delete();
+
+						double sens = 0.0;
+						double spec = 0.0;
+						double prec = 0.0;
+
+						if ((truePositives  + falseNegative) > 0) {
+							sens = (double) truePositives / (truePositives + falseNegative);
+						}
+						if ((trueNegatives + falsePositives) > 0) {
+							spec = (double) trueNegatives / (trueNegatives + falsePositives);
+						}
+						if ((truePositives  + falsePositives) > 0) {
+							prec = (double) truePositives / (truePositives + falsePositives);
+						}
+						double averageDistance = (double) distance / count;
+
+						System.out.println("falsePositives " + falsePositives);
+						System.out.println("sens " + sens);
+						System.out.println("spec " + spec);
+						System.out.println("prec " + prec);
+
+						writer.setDouble(0, setup);
+						writer.setDouble(1, count);
+						writer.setInteger(2, noise);
+						writer.setDouble(3, averageDistance);
+						writer.setDouble(4, sens);
+						writer.setDouble(5, spec);
+						writer.setDouble(6, prec);
+						writer.setDouble(7, 0.5);
+						writer.setInteger(8, h);
+						writer.setInteger(9, w);
+						writer.next();
 					}
 				}
+				// }
 				writer.close();
 			}
 
