4d14cf9b769a75e49e0daee24aa92e61470fd86b
seppinho <sebastian.schoenherr@i-med.ac.at>
2022-12-14 17:53:27 +0100

    Fix format

--- src/core/AnnotationAAC.java
+++ src/core/AnnotationAAC.java
@@ -7,26 +7,27 @@ public class AnnotationAAC {
 	private short codon;
 	private String aminoAcidChange;
 
-	
-	public AnnotationAAC(String pos, String gen, short cod, String aac){
-		position= pos;
+	public AnnotationAAC(String pos, String gen, short cod, String aac) {
+		position = pos;
 		gene = gen;
 		codon = cod;
 		aminoAcidChange = aac;
 	}
-	
+
 	public String getPosition() {
 		return position;
 	}
+
 	public String getGene() {
 		return gene;
 	}
+
 	public short getCodon() {
 		return codon;
 	}
+
 	public String getAminoAcidChange() {
 		return aminoAcidChange;
 	}
-	
-	
+
 }
