ae2b77ab8002610f2554e246f78de41679b153a6
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2022-12-20 10:24:50 +0100

    Reset pyhlotree in testcase

--- tests/export/HsdExportTest.java
+++ tests/export/HsdExportTest.java
@@ -75,7 +75,8 @@ public class HsdExportTest {
 
 		KulczynskiRanking newRanker = new KulczynskiRanking(10);
 		newSampleFile.updateClassificationResults(phylotree, newRanker);
-		
+		HashSet<String> hotspots = new HashSet<>(Arrays.asList("315.1C", "309.1C", "309.1CC", "523d","524d","524.1AC","524.1ACAC","3107d","16182C","16183C","16193.1C","16193.1CC","16519C"));
+		phylotree = PhylotreeManager.getInstance().getPhylotree("phylotree17.xml", "weights17.txt", reference, hotspots);
 		System.out.println("a " +phylotree.getHotspots());
 		System.out.println("b " + phylotree.getPhyloTree().getTree().getHotspots());
 
