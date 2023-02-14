b3dc58240aeb84525e678fb66b009ce4de41701e
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2023-02-14 11:41:27 +0100

    Add FASTA check for nomenclature rules (#22)
    Co-authored-by: haansi <haansi@gmail.com>

--- tests/importer/FastaImporterTests.java
+++ tests/importer/FastaImporterTests.java
@@ -4,14 +4,22 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 import org.junit.Test;
 
+import core.Polymorphism;
 import core.Reference;
 import core.SampleFile;
-import core.TestSample;
+import phylotree.Phylotree;
+import phylotree.PhylotreeManager;
+import qualityAssurance.QualityAssistent;
+import qualityAssurance.RuleSet;
+import qualityAssurance.rules.CheckForTooManyGlobalPrivateMutations;
+import qualityAssurance.rules.CheckForTooManyN;
 import search.ranking.KulczynskiRanking;
+import search.ranking.RankingMethod;
 
 public class FastaImporterTests {
 
@@ -62,7 +70,7 @@ public class FastaImporterTests {
 		ArrayList<String> samples = impFasta.load(new File(file), ref);
 
 		String[] splits = samples.get(0).split("\t");
-		
+
 		for (int i = 3; i < splits.length; i++) {
 			actual.append(splits[i] + ",");
 		}
@@ -157,5 +165,90 @@ public class FastaImporterTests {
 		assertEquals(true, set.contains("8270-8277d"));
 
 	}
-	
+
+	// random shuffle
+	@Test
+	public void testParseSampleWithNRanges() throws Exception {
+		String file = "test-data/fasta/test3.fasta";
+		FastaImporter impFasta = new FastaImporter();
+		Reference ref = new Reference("test-data/reference/rcrs/rCRS.fasta");
+		ArrayList<String> samples = impFasta.load(new File(file), ref);
+
+		String[] splits = samples.get(0).split("\t");
+		HashSet<String> set = new HashSet<String>();
+
+		String range = splits[1];
+		for (int i = 3; i < splits.length; i++) {
+			set.add(splits[i]);
+		}
+
+		assertEquals(true, set.contains("8271-8279d"));
+		assertEquals(true, set.contains("309.1CCT"));
+		assertEquals(true, set.contains("470R"));
+		assertEquals(true, set.contains("499R"));
+		assertEquals(true, set.contains("5068Y"));
+
+	}
+
+	@Test
+	public void testNomenclatureRules() throws Exception {
+		String file = "test-data/fasta/KF450918.fasta";
+		FastaImporter impFasta = new FastaImporter();
+		Reference ref = new Reference("test-data/reference/rcrs/rCRS.fasta");
+		ArrayList<String> samples = impFasta.load(new File(file), ref);
+
+		assertEquals(1, samples.size());
+
+		String[] splits = samples.get(0).split("\t");
+		HashSet<String> set = new HashSet<String>();
+
+		SampleFile sampleFile = new SampleFile(samples, ref);
+
+		HashSet<String> hotspots = new HashSet<>(Arrays.asList("315.1C", "309.1C", "309.1CC", "523d", "524d", "524.1AC", "524.1ACAC", "3107d", "16182C",
+				"16183C", "16193.1C", "16193.1CC", "16519C"));
+
+		Phylotree phylotree = PhylotreeManager.getInstance().getPhylotree("phylotree17.xml", "weights17.txt", ref, hotspots);
+
+		assertEquals(101, sampleFile.getTestSamples().get(0).getSample().getPolymorphisms().size());
+
+		HashSet<String> setBefore = new HashSet<String>();
+
+		for (int i = 0; i < sampleFile.getTestSamples().get(0).getSample().getPolymorphisms().size(); i++) {
+			setBefore.add(sampleFile.getTestSamples().get(0).getSample().getPolymorphisms().get(i) + "");
+		}
+
+		sampleFile.applyNomenclatureRules(phylotree, "test-data/rules/rules.csv");
+
+		HashSet<String> setAfter = new HashSet<String>();
+
+		for (int i = 0; i < sampleFile.getTestSamples().get(0).getSample().getPolymorphisms().size(); i++) {
+			setAfter.add(sampleFile.getTestSamples().get(0).getSample().getPolymorphisms().get(i) + "");
+		}
+
+		HashSet<String> setAfterDiffs = new HashSet<String>(setAfter);
+		HashSet<String> setBeforeDiffs = new HashSet<String>(setBefore);
+
+		setAfterDiffs.removeAll(setBefore);
+		setBeforeDiffs.removeAll(setAfter);
+		
+		//System.out.println(setAfterDiffs.toString());
+		//System.out.println(setBeforeDiffs.toString());
+
+		assertEquals(8, setAfterDiffs.size());
+		assertEquals(9, setBeforeDiffs.size());
+
+
+		//differences due to rule 3107C 3106d -> 3107d)
+		assertEquals(true, setAfter.contains("3107d"));
+		assertEquals(false, setAfter.contains("3106d"));
+		assertEquals(false, setAfter.contains("3107C"));
+
+		assertEquals(true,setBefore.contains("3106d"));
+		assertEquals(true,setBefore.contains("3107C"));
+
+
+		assertEquals(100, sampleFile.getTestSamples().get(0).getSample().getPolymorphisms().size());
+
+	}
+
 }
