674105133ae2a46a3279b8f73797ad1fd1ffcd9f
seppinho <sebastian.schoenherr@i-med.ac.at>
2023-02-23 15:28:17 +0100

    Format code

--- src/qualityAssurance/rules/CheckForAlignmentWarnings.java
+++ src/qualityAssurance/rules/CheckForAlignmentWarnings.java
@@ -15,34 +15,34 @@ import core.TestSample;
 public class CheckForAlignmentWarnings extends HaplogrepRule {
 
 	static final Log log = LogFactory.getLog(CheckForAlignmentWarnings.class);
-	
+
 	public CheckForAlignmentWarnings(int priority) {
 		super(priority);
 	}
 
 	@Override
 	public void evaluate(QualityAssistent qualityAssistent, TestSample currentSample) {
-		if(currentSample.getResults().size() != 0){
+		if (currentSample.getResults().size() != 0) {
 
-		int numAlignWarning = 0;
-		String alignmentProblem="";
-		for(Polymorphism current : currentSample.getSample().getPolymorphisms()){
-			if(current.isReliable()>0)
-				{numAlignWarning++;
-				alignmentProblem+=current+" ("+current.isReliable()+") " ;
+			int numAlignWarning = 0;
+			String alignmentProblem = "";
+			for (Polymorphism current : currentSample.getSample().getPolymorphisms()) {
+				if (current.isReliable() > 0) {
+					numAlignWarning++;
+					alignmentProblem += current + " (" + current.isReliable() + ") ";
 				}
-		}
-		
-		if(numAlignWarning > 0)
-			qualityAssistent.addNewIssue(new AlignmentInfo(qualityAssistent, currentSample, "Alignment check: " + numAlignWarning + " position to recheck: "+alignmentProblem 
-	));
+			}
+
+			if (numAlignWarning > 0)
+				qualityAssistent.addNewIssue(new AlignmentInfo(qualityAssistent, currentSample,
+						"Alignment check: " + numAlignWarning + " position to recheck: " + alignmentProblem));
 		}
 	}
 
 	@Override
 	public void suppressIssues(QualityAssistent qualityAssistent, TestSample currentSample) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
